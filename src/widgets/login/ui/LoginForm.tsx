ee55d8079258bd8630fafc391cb19beb162fbd3a
cho4u4o <cho4u4o@gmail.com>
2025-07-25 16:00:16 +0900

    feat : define type of `Login` type and connect form

--- src/widgets/login/ui/LoginForm.tsx
+++ src/widgets/login/ui/LoginForm.tsx
@@ -5,18 +5,28 @@ import { authFormAtag, authFormButton, authFormPtag } from '@/shared/styles';
 
 import FormInput from '@/shared/components/FormInput';
 import Button from '@/shared/components/Button';
+import { useForm } from 'react-hook-form';
+import type { Login } from '../types';
 
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm<Login>();
+
+  const onSubmit = (data: Login) => {
+    console.log(data);
+  };
+
   return (
-    <form className={authFormWrapper}>
+    <form className={authFormWrapper} onSubmit={handleSubmit(onSubmit)}>
       <div className={authForm}>
-        <FormInput title='이메일 또는 전화번호' />
-        <FormInput title='비밀번호' type='password' />
+        <FormInput title='이메일 또는 전화번호' {...register('email', { required: true })} />
+        <FormInput title='비밀번호' type='password' {...register('password', { required: true })} />
       </div>
       <a href='' className={authFormAtag}>
         비밀번호를 잊으셨나요?
       </a>
-      <Button className={authFormButton}>로그인</Button>
+      <Button className={authFormButton} type='submit'>
+        로그인
+      </Button>
       <p className={authFormPtag}>
         <span>계정이 필요한가요? </span>
         <a href={PATH.REGISTER} className={authFormAtag}>
