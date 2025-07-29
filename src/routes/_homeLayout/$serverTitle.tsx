43dceafe49d7d89cb7ac58b3ea0d919589f4f58b
cho4u4o <cho4u4o@gmail.com>
2025-07-29 10:57:22 +0900

    feat : redirect automatically to chatroom after enter the server

--- src/routes/_homeLayout/$serverTitle.tsx
+++ src/routes/_homeLayout/$serverTitle.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { useEffect } from 'react';
+
+import { CHAT_LIST } from '@/shared/mock';
+import { createFileRoute, Outlet, useNavigate } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_homeLayout/$serverTitle')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+  const { serverTitle } = Route.useParams();
+  const chatRoomId = CHAT_LIST[serverTitle][0].id;
+
+  useEffect(() => {
+    navigate({ to: '/$serverTitle/chat/$chatRoomId', params: { serverTitle, chatRoomId } });
+  }, [navigate, serverTitle]);
+
   return <Outlet />;
 }
