a4894ac80d208632e7c969b21e5902e17a2fa2b0
cho4u4o <cho4u4o@gmail.com>
2025-07-30 18:50:40 +0900

    feat : connect logic with `LoginForm`

--- src/widgets/login/api/submitLogin.ts
+++ src/widgets/login/api/submitLogin.ts
@@ -2,9 +2,10 @@ import { post } from '@/shared/api';
 import { REQUEST } from '@/shared/api/request';
 import { useMutation } from '@tanstack/react-query';
 import type { Login } from '../types';
+import type { Token } from '@/shared/types';
 
 const submitLogin = async ({ email, password }: Login) => {
-  const response = await post<Login>({
+  const response = await post<Login, Token>({
     request: REQUEST.LOGIN,
     data: { email: email, password: password },
   });
