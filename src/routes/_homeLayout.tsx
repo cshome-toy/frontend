c2ac48b69349009ee01482e11d33b15ac1ead294
cho4u4o <cho4u4o@gmail.com>
2025-07-30 19:12:41 +0900

    feat : implement conditional rendering

--- src/routes/_homeLayout.tsx
+++ src/routes/_homeLayout.tsx
@@ -1,8 +1,18 @@
+import { PATH } from '@/shared/constants';
+import { fetchLoginStatus } from '@/shared/utils';
 import { container, mainContent } from '@/shared/styles/container.css';
 import { ChannelSelector, Header, SideBar, UserInfo } from '@/widgets/menu/ui';
-import { Outlet, createFileRoute } from '@tanstack/react-router';
+import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_homeLayout')({
+  beforeLoad: () => {
+    const isAuthenticated = fetchLoginStatus();
+    if (!isAuthenticated) {
+      throw redirect({
+        to: PATH.LOGIN,
+      });
+    }
+  },
   component: () => (
     <div className={container}>
       <Header />
