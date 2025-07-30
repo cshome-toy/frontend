46eb1903a2b2131c3467e66d01fbd9105f10d7df
cho4u4o <cho4u4o@gmail.com>
2025-07-30 12:22:32 +0900

    feat : implement submitting login logic

---
+++ src/widgets/login/api/submitLogin.ts
@@ -0,0 +1,18 @@
+import { post } from '@/shared/api';
+import { REQUEST } from '@/shared/api/request';
+import { useMutation } from '@tanstack/react-query';
+import type { Login } from '../types';
+
+const submitLogin = async ({ email, password }: Login) => {
+  const response = await post<Login>({
+    request: REQUEST.LOGIN,
+    data: { email: email, password: password },
+  });
+  return response.data;
+};
+
+export const useSubmitLogin = () => {
+  return useMutation({
+    mutationFn: submitLogin,
+  });
+};
